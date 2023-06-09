f2a319c54c7c8bf94ec6167b73f59c53d7ac4740
Jesher <jesherjesher1@gmail.com>
2023-06-09 13:24:12 -0600

    02 Variables

---
+++ variable.py
@@ -0,0 +1,13 @@
+# Python es no Tipado
+
+name = "Jesher Mendieta Ibarra"
+age = 18
+height = 1.75
+is_student = True
+children = []
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(children))
