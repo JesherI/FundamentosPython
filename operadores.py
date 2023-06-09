823f410e043a681e6edcc55a0c393cf852bd5ef8
Jesher <jesherjesher1@gmail.com>
2023-06-09 13:59:30 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+# Operadores Matematicos
+# + - * %
+#/ división float
+#// División entera
+#** Potencia
+
+print(2+2)
+print(2-2)
+print(2*2)
+print(2%2)
+print(10/3)
+print(10//3)
+print(3**3)
+
+#operadores para String
+# + (concatena) * (replica)
+
+print("Hola " + "Mundo!")
+print("Hola \n " * 4)
