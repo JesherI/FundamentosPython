51e1b262cd65173c1f2e50a5e848a7f6456f34c3
Jesher <jesherjesher1@gmail.com>
2023-06-14 11:27:27 -0600

    09 Tuplas y Conjuntos

---
+++ colecciones-2.py
@@ -0,0 +1,12 @@
+# Tuplas
+my_tuple = ('Hola',12,1.5,True)
+print(type(my_tuple))
+print(my_tuple[0])
+print(my_tuple[-1])
+#Error
+#my_tuple[0]="otro valor"
+
+# Cunjuntos (set)
+my_set = {1,2,3,4,4,4,4,True,'hi'}
+print(type(my_set))
+print(my_set)
